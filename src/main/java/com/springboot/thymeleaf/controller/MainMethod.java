64e6e8d5dbb04c740c1557337d83cd27b0b93da3
Halder <681019@cognizant.com>
2018-03-28 19:02:43 +0530

    springboot thymeleaf with create a form_personlist

---
+++ src/main/java/com/springboot/thymeleaf/controller/MainMethod.java
@@ -0,0 +1,19 @@
+package com.springboot.thymeleaf.controller;
+
+import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+	public class MainMethod {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			SpringApplication.run(MainMethod.class, args);
+		}
+
+	}
+
+
